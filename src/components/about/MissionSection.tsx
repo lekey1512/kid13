9334b41e83e5bf17d686cf69912662805c4ea4e1
lekey1512 <lekey1512@gmail.com>
2026-09-08 11:20:11 +0700

    Updated MissionSection.tsx

--- src/components/about/MissionSection.tsx
+++ src/components/about/MissionSection.tsx
@@ -1,7 +1,7 @@
 import { Sparkles, BookOpen, Heart, Users } from 'lucide-react';
 
 const MISSION_PHOTO =
-  'https://res.cloudinary.com/wsaz946u/image/upload/v1788167396/kindy_testimonial_right.webp';
+  'https://res.cloudinary.com/wsaz946u/image/upload/v1788167396/kindy_tranbaokhang.webp';
 
 interface MissionCard {
   icon: typeof BookOpen;
