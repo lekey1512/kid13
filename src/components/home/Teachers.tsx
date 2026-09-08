3d76f08869633c56b4d521c8769a959e828cacfc
lekey1512 <lekey1512@gmail.com>
2026-09-08 11:36:08 +0700

    Updated Teachers.tsx

--- src/components/home/Teachers.tsx
+++ src/components/home/Teachers.tsx
@@ -30,7 +30,7 @@ const teachers: Teacher[] = [
     name: 'Tirich Mohamed',
     initial: 'T',
     photo:
-      'https://res.cloudinary.com/wsaz946u/image/upload/v1788167433/kindy_avta-RickDujister.webp',
+      'https://res.cloudinary.com/wsaz946u/image/upload/v1788167432/kindy_avta-PavelKukikov.webp',
     credential: 'TESOL – Chứng chỉ giảng dạy chuyên nghiệp',
     experience: '10 năm kinh nghiệm · Chuyên IELTS',
     tag: 'Giáo viên bản ngữ',
