db610d3870aaa2d13dda4fbb52ac645e1302505b
lekey1512 <lekey1512@gmail.com>
2026-09-08 11:52:01 +0700

    Updated TeacherGallery.tsx

--- src/components/teachers/TeacherGallery.tsx
+++ src/components/teachers/TeacherGallery.tsx
@@ -39,7 +39,7 @@ const baseTeachers: Teacher[] = [
     name: 'Bích Sơn',
     initial: 'B',
     photo:
-      'https://res.cloudinary.com/wsaz946u/image/upload/v1788167400/kindy_hotantinhanh.webp',
+      'https://res.cloudinary.com/wsaz946u/image/upload/v1788167424/kindy_tes_1.webp',
     credential: 'TESOL - Chứng chỉ giảng dạy chuyên nghiệp',
     experience: '05 năm kinh nghiệm · Chuyên IELTS',
     tag: 'Giáo viên Việt Nam',
