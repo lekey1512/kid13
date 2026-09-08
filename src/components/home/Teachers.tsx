e0b121441d4a4b29c4e91adacc4da5353261350c
lekey1512 <lekey1512@gmail.com>
2026-09-08 11:34:00 +0700

    Updated Teachers.tsx

--- src/components/home/Teachers.tsx
+++ src/components/home/Teachers.tsx
@@ -62,7 +62,7 @@ const teachers: Teacher[] = [
     name: 'Ngọc Minh',
     initial: 'N',
     photo:
-      'https://res.cloudinary.com/wsaz946u/image/upload/v1788167397/kindy_vudanhthanh.webp',
+      'https://res.cloudinary.com/wsaz946u/image/upload/v1788167430/primary_tes_2.webp',
     experience: '03 năm kinh nghiệm · IELTS 7.0',
     tag: 'Giáo viên Việt Nam',
     isNative: false,
