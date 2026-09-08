997f4efa32f663d5b6d82f4e6ca2333175e58ba4
lekey1512 <lekey1512@gmail.com>
2026-09-08 11:46:48 +0700

    Updated CourseOverview.tsx

--- src/components/programs/teens/CourseOverview.tsx
+++ src/components/programs/teens/CourseOverview.tsx
@@ -3,7 +3,7 @@ import { Sparkles, Star } from 'lucide-react';
 const PRIMARY_PHOTO =
   'https://res.cloudinary.com/wsaz946u/image/upload/v1788167373/hs-vo-tran-song-thu.png';
 const SECONDARY_PHOTO =
-  'https://res.cloudinary.com/wsaz946u/image/upload/v1788167378/hs-kim-anh-khoi.png';
+  'https://res.cloudinary.com/wsaz946u/image/upload/v1788167372/hs-nguyen-hoang-ngan.png';
 const BG_IMAGE =
   'https://res.cloudinary.com/wsaz946u/image/upload/v1788166535/kindy_bg_form_top.webp';
 const ANIMAL =
