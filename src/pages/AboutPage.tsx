e41b48bb062f47d2451843faa337f74361261e96
lekey1512 <lekey1512@gmail.com>
2026-09-08 11:52:53 +0700

    Updated AboutPage.tsx

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -10,7 +10,7 @@ export default function AboutPage() {
         badge="Về chúng tôi"
         title="Giới thiệu ILE"
         subtitle="Hành trình kiên định với sứ mệnh mang tiếng Anh đến gần hơn với trẻ em Việt Nam."
-        image="https://res.cloudinary.com/wsaz946u/image/upload/v1788167404/kindy_little-girls-section-sun-little-kids.webp"
+        image="https://res.cloudinary.com/wsaz946u/image/upload/v1788167435/kindy_slide2.webp"
         imageAlt="Học viên ILE"
         fullBleed={true}
       />
